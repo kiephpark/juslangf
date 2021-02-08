a0270a2a6be88decfe424b9217aed06d255ee7fa
parkunchol <kiephpark@gmail.com>
2021-02-08 16:25:27 +0900

    applied https setting

--- api/http-common.js
+++ api/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://api.localhost:8000/",
-  baseURL: "http://api.juslang.ml:8000/",
+  baseURL: "https://api.juslang.ml/",
   // baseURL: "https://api.sdi2nd.ml/",
   headers: {
     "Content-type": "application/json",
